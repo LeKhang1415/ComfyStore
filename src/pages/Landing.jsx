7b6a91557c76026aab9ee2f415ee8be3d1d4c6ac
khang <chaoskid1415@gmail.com>
2025-07-06 16:20:55 +0700

    About page & Hero component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,11 @@
+import { Hero } from "../components";
+
 function Landing() {
-    return <h1 className="text-4xl">HomeLayout</h1>;
+    return (
+        <>
+            <Hero />
+        </>
+    );
 }
 
 export default Landing;
