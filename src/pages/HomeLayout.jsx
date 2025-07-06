ae2854ceeed511455fc335f45acf4ccbf7968383
khang <chaoskid1415@gmail.com>
2025-07-06 15:19:11 +0700

    Header component

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,10 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "../components";
 
 function HomeLayout() {
     return (
         <>
+            <Header />
             <section className="align-element py-20">
-                {" "}
                 <Outlet />
             </section>
         </>
