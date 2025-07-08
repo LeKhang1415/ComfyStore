5ddc1d83d92c51878f41d6f2aa9787e023f0fe71
khang <chaoskid1415@gmail.com>
2025-07-08 15:44:33 +0700

    Filter component

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,18 +1,16 @@
-function FormInput({ label, name, type, defaultValue, size }) {
+const FormInput = ({ label, name, type, defaultValue, size }) => {
     return (
         <div className="form-control">
-            <label className="label ">
+            <label htmlFor={name} className="label">
                 <span className="label-text capitalize">{label}</span>
             </label>
             <input
-                name={name}
                 type={type}
-                placeholder="Type here"
-                className={`input input-bordered ${size}`}
+                name={name}
                 defaultValue={defaultValue}
+                className={`input input-bordered ${size}`}
             />
         </div>
     );
-}
-
+};
 export default FormInput;
